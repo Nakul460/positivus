71aec7bf44a3c75a51f96c3f58a89dd49422071a
Nakul460 <141560694+Nakul460@users.noreply.github.com>
2025-04-19 12:28:24 +0530

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function togglemenu() {
+    const navbar = document.querySelector(".nav-bar");
+    navbar.classList.toggle("active");
+    const menubar = document.querySelector(".menu");
+    menubar.classList.toggle("active");
+}
